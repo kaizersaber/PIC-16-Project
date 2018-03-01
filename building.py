3577d87941f69a7d7096df863cc5c4dbfc7332f3
ryangrg <36679263+ryangrg@users.noreply.github.com>
2018-02-28 17:46:13 -0800

    Update building.py
    Added central segments which will become the inside of dorm rooms.

--- building.py
+++ building.py
@@ -4,18 +4,19 @@ Created on Tue Feb 27 21:38:38 2018
 
 @author: Ryan, Tan
 """
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtCore
 import random
 
 class Building(object):
-    def __init__(self, start_x=1000, start_y=5):
+    def __init__(self, start_x=1000, start_y=5, buildingWidth=200):
         self.scaleFactor = 10.0
         self.start_x = start_x
         self.x = start_x
         self.y = start_y
         self.v_x = -15.0
         self.v_y = 0.0
-        self.gapHeightList = [50, 150, 250]
+        self.buildingWidth = buildingWidth
+        self.gapHeightList = [50, 200, 350]
         self.gapHeight = random.choice(self.gapHeightList)
     
     def update(self):
@@ -35,11 +36,10 @@ class Building(object):
             self.scaleFactor = 2.5
     
     def paintBuilding(self, painter):
-        gapSize = 200
-        buildingWidth = 175
+        gapSize = 150
         self.upper_x = self.x
         self.upper_y = self.y
-        self.upper_w = buildingWidth
+        self.upper_w = self.buildingWidth
         self.upper_h = self.gapHeight
         self.upper = painter.fillRect(self.upper_x, 
                                       self.upper_y, 
@@ -48,7 +48,7 @@ class Building(object):
                                       QtCore.Qt.darkRed)
         self.middle_x = self.x
         self.middle_y = self.y+self.gapHeight
-        self.middle_w = buildingWidth
+        self.middle_w = self.buildingWidth
         self.middle_h = gapSize
         self.middle = painter.fillRect(self.middle_x,
                                        self.middle_y,
@@ -58,7 +58,7 @@ class Building(object):
         
         self.lower_x = self.x
         self.lower_y = self.y+self.gapHeight+gapSize
-        self.lower_w = buildingWidth
+        self.lower_w = self.buildingWidth
         self.lower_h = 706-(self.y+self.gapHeight+gapSize)
         self.lower = painter.fillRect(self.lower_x, 
                                       self.lower_y, 
